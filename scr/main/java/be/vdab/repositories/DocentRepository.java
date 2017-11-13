7ce744f641a34c066f31c99a6548f9eec1ef8e7b
VDABHEVERLEE\marc.de.jonge <marc.de.jonge1@telenet.be>
2017-11-13 13:30:56 +0100

    Entity Toevoegen

--- scr/main/java/be/vdab/repositories/DocentRepository.java
+++ scr/main/java/be/vdab/repositories/DocentRepository.java
@@ -8,6 +8,10 @@ import be.vdab.entities.Docent;
 
 public class DocentRepository {
 	public Optional<Docent> read(long id, EntityManager entityManager) {
-			return Optional.ofNullable(entityManager.find(Docent.class, id));
+		return Optional.ofNullable(entityManager.find(Docent.class, id));
+	}
+
+	public void create(Docent docent, EntityManager entityManager) {
+		entityManager.persist(docent);
 	}
 }
